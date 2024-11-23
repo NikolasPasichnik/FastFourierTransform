bf3be56107cf1cc8fb7831a559c151455dae65cc
NikolasPasichnik <nikolas.pasichnik@mail.mcgill.ca>
2024-11-23 14:04:34 -0500

    implemented the image size adjustment function

--- fft.py
+++ fft.py
@@ -1,5 +1,8 @@
 import argparse
-
+import numpy as np
+import matplotlib.image as pltimage
+import cv2
+import math 
 
 def parse_input():
     '''
@@ -16,8 +19,47 @@ def parse_input():
 
     return parser.parse_args()
 
+# Adjusting the size of the inputted image to make sure the algorithms work. 
+def get_adjusted_dimensions(image_path):
+    image_array = cv2.imread(image_path)
+
+    height_power = math.ceil(math.log2(len(image_array)))
+    width_power = math.ceil(math.log2(len(image_array[0])))
+
+    adjusted_height = pow(2,height_power)
+    adjusted_width = pow(2,width_power)
+
+    adjusted_image_array = cv2.resize(image_array, (adjusted_width,adjusted_height))
+
+    return adjusted_image_array
+
+# Discrete fourier transform -> X_k = sum{n: 0 to N-1}(x_n * e^(-i2{pi}kn/N)) for k = 0 to N-1
+def naive_dft_1D(image_array):
+    print("naive_dft_1D")
+
+# Discrete fourier transform (inverse) -> x_n = 1/N * sum{k: 0 to N-1}(X_n * e^(i2{pi}kn/N)) for k = 0 to N-1
+def naive_inverse_dft_1D(image_array):
+    print("naive_inverse_dft_1D")
+
+# check instructions 
+def naive_dft_2D(image_array):
+    print("naive_dft_2D")
+
 
 if __name__ == "__main__":
     args = parse_input()
+    # Getting input 
     print(args.mode)
     print(args.image)
+
+    # Original image dimensions
+    image_array = cv2.imread(args.image)
+    print("original dimensions:")
+    print(len(image_array)) #height
+    print(len(image_array[0])) #width 
+    
+    # Adjusted image dimensions 
+    adjusted_image_array = get_adjusted_dimensions(args.image)
+    print("adjusted dimensions:")
+    print(len(adjusted_image_array))
+    print(len(adjusted_image_array[0]))
